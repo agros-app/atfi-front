20a262cedc72df2159d0578fb72af5eb692670d0
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-10 04:11:30 -0300

    add button component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,7 +1,32 @@
+import { MouseEventHandler } from "react";
+import styles from "./button.module.scss";
+
 type ButtonProps = {
   children: React.ReactNode;
+  variant?: "primary" | "secondary" | "ghost" | "outlined" | "custom";
+  size?: "sm" | "md" | "lg";
+  className?: string;
+  disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+export default function Button({
+  children,
+  variant = "primary",
+  size = "lg",
+  className = "",
+  disabled,
+  onClick,
+}: ButtonProps) {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`${styles.button} ${styles[variant]} ${styles[size]} ${
+        disabled ? styles.disabled : ""
+      } ${className}`}
+    >
+      {children}
+    </button>
+  );
 }
