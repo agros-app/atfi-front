a55ed8f627d0f7f25be4e4e0cddedd9eb9959430
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-10 04:11:41 -0300

    add textfield component

---
+++ components/textField/textField.tsx
@@ -0,0 +1,31 @@
+import styles from "./textField.module.scss";
+
+type TextFieldProps = {
+  placeholder: string;
+  name: string;
+  className?: string;
+  label?: string;
+  helperText?: string;
+  error?: boolean;
+};
+
+export default function TextField({
+  placeholder,
+  name,
+  className,
+  label,
+  helperText,
+  error,
+}: TextFieldProps) {
+  return (
+    <div className={`${styles.container} ${error ? styles.error : ""}`}>
+      {label && (
+        <label className={styles.label} htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <input type="text" placeholder={placeholder} name={name} />
+      {helperText && <small className={styles.helperText}>{helperText}</small>}
+    </div>
+  );
+}
