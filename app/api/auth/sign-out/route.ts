d5eb42e0ae95f666c392f3bc8475812937941cf2
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-15 11:44:20 -0300

    add log out functionality

--- app/api/auth/sign-out/route.ts
+++ app/api/auth/sign-out/route.ts
@@ -1,15 +1,16 @@
 import { auth } from "@/lib/firebaseAuth";
+import { deleteSession, getSession } from "@/lib/session";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   // TODO: test this route
-  const sessionCookie = cookies().get("__session")?.value;
+  const sessionCookie = getSession();
 
   if (!sessionCookie)
     return NextResponse.json({ success: false, error: "Session not found." }, { status: 400 });
 
-  cookies().delete("__session");
+  deleteSession();
 
   await auth.signOut();
 
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import Logo from "@/assets/icons/logo";
 import Link from "next/link";
 import styles from "./navbar.module.scss";
 import { getSession } from "@/lib/session";
-import Button from "../button/button";
+import LogOutButton from "../log_out_button/LogOutButton";
 
 export default async function NavBar() {
   const user = await getSession();
@@ -16,11 +16,7 @@ export default async function NavBar() {
       <div className={styles.right}>
         <Link href="/projects">Proyectos</Link>
         <Link href="/simulator">Simulador</Link>
-        {user ? (
-          <Button>Cerrar Sesión</Button>
-        ) : (
-          <Link href="/login">Ingresar</Link>
-        )}
+        {user ? <LogOutButton /> : <Link href="/login">Ingresar</Link>}
       </div>
     </nav>
   );
