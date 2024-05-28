353adcac34a3389e0bad491d1fffa993e2809842
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2024-05-28 15:21:46 -0300

    Landing card component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import NavBar from "@/components/navbar/navbar";
-import LandingCard from "@/components/landingCard/landingCard";
 import seedling from "@/assets/iconsPng/seedling.png";
 export default function Home() {
   return (
@@ -8,11 +7,6 @@ export default function Home() {
       <NavBar />
       <main>
         <h1>atfi</h1>
-              <LandingCard
-                  icon={seedling.src}
-                  title={"Explorá"}
-                  description={"En nuestra plataforma encontrarás diversas oportunidades de inversión disponibles."}
-              ></LandingCard>
       </main>
     </>
   );
