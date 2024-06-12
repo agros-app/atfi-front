fc097483781c20019db9ffe0848350841ffc726a
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-06-12 12:51:37 -0300

    fix: button variant

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -3,7 +3,7 @@ import styles from "./button.module.scss";
 
 type ButtonProps = {
   children: React.ReactNode;
-  variant?: "primary" | "secondary" | "tertiary" | "ghost" | "outlined";
+  variant?: "primary" | "secondary" | "tertiary" | "ghost" | "outlined" | "custom";
   size?: "sm" | "md" | "lg";
   fill?: boolean;
   className?: string;
