28fcd3762747f1f2363013aee6db358811fa9aad
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-06-15 21:53:32 -0300

    addded features section

--- app/(with-navbar)/landing/components/howItWorks/howItWorks.tsx
+++ app/(with-navbar)/landing/components/howItWorks/howItWorks.tsx
@@ -29,6 +29,27 @@ const comoFunciona = [
     },
 ];
 
+const vision = 'ATFI visualiza un futuro donde la financiación agrícola sea democratizada y transparente. Al liderar la integración de la tecnología de criptomonedas en los modelos de financiación tradicionales, ATFI busca derribar barreras y empoderar a las personas para que participen en la economía agrícola como nunca antes. A través de una adaptación continua a las necesidades del mercado, ATFI aspira a revolucionar la forma en que se financia la agricultura, garantizando que los recursos se asignen de manera eficiente para el beneficio tanto de los inversores como de los productores agropecuarios.'
+
+const featureList = [
+    {
+        title: 'Transparencia',
+        description: 'ATFI visualiza un futuro con una financiación agrícola accesible y clara. La transparencia y la trazabilidad son fundamentales para nosotros, por lo que proporcionamos actualizaciones periódicas y detalladas sobre el progreso de cada proyecto.'
+    },
+    {
+        title: 'Retornos competitivos',
+        description: 'Ofrece retornos competitivos y transparentes a los inversores. Desde el momento en que realizas tu inversión, puedes seguir el progreso de la inversion a través de nuestra plataforma con informacion en tiempo real.'
+    },
+    {
+        title: 'Innovación tecnológica',
+        description: ' Lidera la integración de criptomonedas en modelos de financiación tradicionales. Mediante la adopción de tecnologías innovadoras, ATFI busca derribar barreras y empoderar a las personas para que participen en la economía agrícola como nunca antes.'
+    },
+    {
+        title: 'Oportunidades',
+        description: 'Facilita el acceso de los agricultores a mercados globales, mejorando sus oportunidades de venta. Brinda a los agricultores la posibilidad de acceder a financiación a tasas competitivas, lo que les permite expandir sus operaciones y aumentar su producción.'
+    },
+];
+
 const containerVariants = {
     hidden: { opacity: 0, y: 50 },
     visible: {
@@ -47,10 +68,44 @@ const itemVariants = {
     visible: { opacity: 1, y: 0, transition: { duration: 0.75, delay: 0.2 } },
 };
 
+const visionContainerVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.75,
+            delay: 0.75
+        },
+    },
+};
+
+const visionBorderVariants = {
+    hidden: { width: '0%' },
+    visible: {
+        width: '40%',
+        transition: {
+            duration: 1,
+            ease: "easeInOut"
+        }
+    }
+};
+
 export default function HowItWorks() {
     const controls = useAnimation();
     const ref = useRef(null);
     const isInView = useInView(ref);
+
+    const Feature = ({ title, description }: any) => (
+        <motion.div
+            className={styles.feature}>
+            <h2 className={styles.featureTitle}>{title}</h2>
+            <div className={styles.featureDescription}>
+                <p>{description}</p>
+            </div>
+        </motion.div>
+    );
+
 
     useEffect(() => {
         if (isInView) {
@@ -58,30 +113,44 @@ export default function HowItWorks() {
         }
     }, [controls, isInView]);
 
+
     return (
-        <section className={styles.container} ref={ref}>
+        <section className={styles.container}>
             <motion.div
                 initial="hidden"
                 animate={controls}
                 variants={containerVariants}
                 className={styles.howItWorksContainer}
             >
-                <motion.h1
-                    variants={itemVariants}
-                    className={styles.howItWorks}
-                >
-                    ¿Cómo funciona?
-                </motion.h1>
                 <motion.p
+                    ref={ref}
                     variants={itemVariants}
                     className={styles.howItWorksDescription}
                 >
                     Descubrí una forma sencilla y efectiva de hacer crecer tus finanzas. Te guiamos
                     a través de cada paso para que puedas invertir con confianza y obtener los mejores resultados.
                 </motion.p>
+                <motion.div
+                    initial="hidden"
+                    animate={controls}
+                    variants={visionBorderVariants}
+                    className={styles.visionBorder}
+                />
+                <motion.div
+                    initial="hidden"
+                    animate={controls}
+                    variants={visionContainerVariants}
+                    className={styles.visionContainer}
+                >
+                    {
+                        featureList.map((item, index) => (
+                            <Feature key={index} title={item.title} description={item.description}/>
+                        ))
+                    }
+                </motion.div>
             </motion.div>
             <div className={styles.cards}>
-                {
+            {
                     comoFunciona.map((item, index) => {
                         const cardControls = useAnimation();
                         const cardRef = useRef(null);
@@ -111,3 +180,4 @@ export default function HowItWorks() {
         </section>
     );
 }
+
