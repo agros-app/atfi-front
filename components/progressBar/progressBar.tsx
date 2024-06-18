5c978e72ddc9b55148922489ffbdf6b364ff11d5
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-06-17 21:18:09 -0300

    update project card

--- components/progressBar/progressBar.tsx
+++ components/progressBar/progressBar.tsx
@@ -1,25 +1,18 @@
-import styles from './progressBar.module.scss';
+import styles from "./progressBar.module.scss";
 
 type ProgressBarProps = {
-    collected: number;
-    goal: number;
-}
-export default function ProgressBar(
-    {
-        collected,
-        goal
-    }: ProgressBarProps)
-{
+  collected: number;
+  goal: number;
+};
+export default function ProgressBar({ collected, goal }: ProgressBarProps) {
+  const percentage = (collected / goal) * 100;
 
-    function percentage(){
-    return (collected/goal)*100;
-    }
-
-    return (
-        <div>
-            <div className={styles.progressBarOutside}>
-                <div className={styles.progressBarInside} style={{width: `${percentage()}%`}} />
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.progressBarOutside}>
+      <div
+        className={styles.progressBarInside}
+        style={{ width: `${percentage}%` }}
+      />
+    </div>
+  );
 }
