00a513cfff8c78a6bb824132222f5f13aa2921e1
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-06-18 19:18:18 -0300

    added framer animations and minor changes

--- components/projectCard/projectCard.tsx
+++ components/projectCard/projectCard.tsx
@@ -13,56 +13,59 @@ import ProgressBar from "../progressBar/progressBar";
 export default function ProjectCard() {
   const router = useRouter();
   return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <Image
-          src={"/farm-image.png"}
-          alt="Farm Image"
-          fill
-          priority
-          style={{ objectFit: "cover" }}
-          sizes="100%"
-        />
-      </div>
-      <div className={styles.bottom}>
-        <div className={styles.roi}>400% (ROI)</div>
-        <div className={styles.info}>
-          <h3>Valle verde</h3>
-          <div className={styles.description}>
-            <div className={styles.specific}>
-              <LocationIcon />
-              <span>Argentina</span>
-            </div>
-            <div className={styles.specific}>
-              <TimeIcon />
-              <span>20 días</span>
-            </div>
-            <div className={styles.specific}>
-              <SeedIcon />
-              <span>Trigo, maíz, soja</span>
-            </div>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <Image
+              src={"/farm-image.png"}
+              alt="Farm Image"
+              fill
+              priority
+              style={{ objectFit: "cover", filter:'brightness(0.9)' }}
+              sizes="100%"
+          />
+          <div className={styles.imageOverlay}>
+            <h3 className={styles.title}>Valle verde</h3>
+            <p className={styles.titleDescription}>Valle verde, 123</p>
           </div>
         </div>
-        <div className={styles.financial}>
-          <div className={styles.goal}>
-            <div>
-              <span className={styles.label}>Actual</span>
-              <span className={styles.value}>$500.000</span>
-            </div>
-            <div>
-              <span className={styles.label}>Meta</span>
-              <span className={styles.value}>$1.000.000</span>
+        <div className={styles.bottom}>
+          <div className={styles.roi}>400% (ROI)</div>
+          <div className={styles.info}>
+            <div className={styles.description}>
+              <div className={styles.specific}>
+                <LocationIcon />
+                <span>Argentina</span>
+              </div>
+              <div className={styles.specific}>
+                <TimeIcon />
+                <span>20 días</span>
+              </div>
+              <div className={styles.specific}>
+                <SeedIcon />
+                <span>Trigo, maíz, soja</span>
+              </div>
             </div>
           </div>
-          <div className={styles.progress}>
-            <span>50%</span>
-            <ProgressBar collected={500000} goal={1000000} />
+          <div className={styles.financial}>
+            <div className={styles.goal}>
+              <div>
+                <span className={styles.label}>Actual</span>
+                <span className={styles.value}>$500.000</span>
+              </div>
+              <div>
+                <span className={styles.label}>Meta</span>
+                <span className={styles.value}>$1.000.000</span>
+              </div>
+            </div>
+            <div className={styles.progress}>
+              <span>50%</span>
+              <ProgressBar collected={500000} goal={1000000} />
+            </div>
           </div>
+          <Button size="sm" onClick={() => router.push("/project/1")}>
+            Invertir
+          </Button>
         </div>
-        <Button size="sm" onClick={() => router.push("/project/1")}>
-          Invertir
-        </Button>
       </div>
-    </div>
   );
 }
