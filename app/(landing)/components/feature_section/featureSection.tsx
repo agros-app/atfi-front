1da2d5c9e6eae6743a4849ef0ec737fda4445d69
Gabriel Giana <92042855+GGCodeLatam@users.noreply.github.com>
2024-07-09 14:34:27 -0300

    Update featureSection.tsx
    Update text

--- app/(landing)/components/feature_section/featureSection.tsx
+++ app/(landing)/components/feature_section/featureSection.tsx
@@ -11,24 +11,24 @@ import {
 
 const featureList = [
   {
-    title: "Transparencia",
+    title: "Transparencia y Trazabilidad",
     description:
-      "ATFI visualiza un futuro con una financiación agrícola accesible y clara. La transparencia y la trazabilidad son fundamentales para nosotros, por lo que proporcionamos actualizaciones periódicas y detalladas sobre el progreso de cada proyecto.",
+      "Blockchain no es un capricho. No lo utilizamos para tokenizar, es una herramienta que nos permite trazar y mostrar el camino del dinero y los datos de manera segura y trasparente.",
   },
   {
-    title: "Retornos competitivos",
+    title: "Diversificación",
     description:
-      "Ofrece retornos competitivos y transparentes a los inversores. Desde el momento en que realizas tu inversión, puedes seguir el progreso de la inversion a través de nuestra plataforma con informacion en tiempo real.",
+      "La inversión en activos biologicos es una manera de diversificar tu cartera. Es decir, disminuir el riesgo total de la misma.",
   },
   {
     title: "Innovación tecnológica",
     description:
-      " Lidera la integración de criptomonedas en modelos de financiación tradicionales. Mediante la adopción de tecnologías innovadoras, ATFI busca derribar barreras y empoderar a las personas para que participen en la economía agrícola como nunca antes.",
+      "Lidera la integracion de blockchain en modelos de financiacion tradiconales, mediante la adopcion de tecnologias innovadoras. ATFI busca derribar barreras geograficas y dar una opcion de acceso al sector.",
   },
   {
-    title: "Oportunidades",
+    title: "Seguridad Juridica",
     description:
-      "Facilita el acceso de los agricultores a mercados globales, mejorando sus oportunidades de venta. Brinda a los agricultores la posibilidad de acceder a financiación a tasas competitivas, lo que les permite expandir sus operaciones y aumentar su producción.",
+      "ATFI tiene un compromiso inquebrantable para con su ecosistema de respetar los marcos legales y regulatorios, es por eso que cada proyecto se encuadra bajo una figura de fideicomiso.",
   },
 ];
 
@@ -42,9 +42,7 @@ export default function FeatureSection() {
         variants={itemVariants}
         className={styles.description}
       >
-        Descubrí una forma sencilla y efectiva de hacer crecer tus finanzas. Te
-        guiamos a través de cada paso para que puedas invertir con confianza y
-        obtener los mejores resultados.
+      Atfi es una plataforma digital que te permite acceder a los rendimientos del sector agropecuario asociándote a una campaña productiva.
       </motion.p>
       <motion.div
         initial="hidden"
