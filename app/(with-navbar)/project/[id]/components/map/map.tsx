a4a5b67d6c242a44db03f2db16c197adbae2e807
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-08-09 14:23:58 -0300

    feat: add basic implementation of map

--- app/(with-navbar)/project/[id]/components/map/map.tsx
+++ app/(with-navbar)/project/[id]/components/map/map.tsx
@@ -1,5 +1,8 @@
 import Title from '../title/title';
 import styles from './map.module.scss';
+import dynamic from "next/dynamic"
+
+const MapView = dynamic(() => import("@/components/map/MapView"), { ssr:false })
 
 export default function Map(){
     return (
@@ -9,7 +12,7 @@ export default function Map(){
                 <div className={styles.location}>13000 Montevideo, Departamento de Montevideo</div>
             </div>
             <div className={styles.bottomSide}>
-                <img src={"/sampleMap.png"} alt={"Estadio UNO location"}></img>
+                <MapView/>
             </div>
         </div>
     );
