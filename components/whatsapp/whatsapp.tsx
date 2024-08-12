95561f3a38f345eebbb4ad6f52489fe37a4d97b9
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-08-11 22:02:47 -0300

    two tabs added in project page:  - details  - progress

--- components/whatsapp/whatsapp.tsx
+++ components/whatsapp/whatsapp.tsx
@@ -4,14 +4,16 @@ import styles from "./whatsapp.module.scss";
 export default function Whatsapp() {
   return (
     <div className={styles.container}>
-      <img
-        src={"/whatsapp_icon.png"}
-        alt="Whatsapp Icon"
-        className={styles.image}
-      />
-      <div className={styles.info}>
-        <p className={styles.boldText}>¿Te podemos ayudar en algo?</p>
-        <p>Lunes a Viernes de 8 a 16 horas</p>
+        <a href="https://wa.me/541132568564" target="_blank" rel="noopener noreferrer">
+            <img
+                src={"/whatsapp_icon.png"}
+                alt="Whatsapp Icon"
+                className={styles.image}
+            />
+        </a>
+        <div className={styles.info}>
+            <p className={styles.boldText}>¿Te podemos ayudar en algo?</p>
+            <p>Lunes a Viernes de 8 a 16 horas</p>
       </div>
     </div>
   );
