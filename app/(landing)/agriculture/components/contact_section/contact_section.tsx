fa241e67ea024f77b50ca62c81bf585d771d55bc
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2024-08-13 14:03:45 -0300

    Fix: typo

--- app/(landing)/agriculture/components/contact_section/contact_section.tsx
+++ app/(landing)/agriculture/components/contact_section/contact_section.tsx
@@ -5,7 +5,7 @@ import TextField from "@/components/textField/textField";
 const ContactForm = () => {
     return (
         <div className={styles.contactFormContainer}>
-            <h2 className={styles.heading}>Comenzá la transformacion digital de tu campaña agricula</h2>
+            <h2 className={styles.heading}>Comenzá la transformacion digital de tu campaña agricola</h2>
             <div className={styles.content}>
                 <div className={styles.imageContainer}>
                     <img
