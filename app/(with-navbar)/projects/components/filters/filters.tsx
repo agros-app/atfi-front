cbe9198646633eda7d943e282323fcc9a96a8801
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-09-08 12:55:20 -0300

    filters working

--- app/(with-navbar)/projects/components/filters/filters.tsx
+++ app/(with-navbar)/projects/components/filters/filters.tsx
@@ -1,37 +1,56 @@
+import { useState } from "react";
 import styles from "./filters.module.scss";
 
-export default function Filters() {
-  // TODO: Integrate backend
-  return (
-    <div>
-      <h2>Proyectos</h2>
-      <div className={styles.categories}>
-      <div className={styles.category}>
-        <h4>Ubicación</h4>
-        <p>
-          Argentina <b>(4)</b>
-        </p>
-        <p>
-          Uruguay <b>(1)</b>
-        </p>
-        <p>
-          Chile <b>(2)</b>
-        </p>
-      </div>
-      <div className={styles.category}>
-        <h4>Tipo de cosecha</h4>
-        <p>
-          Trigo <b>(4)</b>
-        </p>
-        <p>
-          Soja <b>(1)</b>
-        </p>
-        <p>
-          Girasol <b>(2)</b>
-        </p>
-      </div>
-      </div>
-      
-    </div>
-  );
+interface FiltersProps {
+    onFilterChange: (filter: { country: string | null; type: string | null }) => void;
+    countries: string[];
+    types: string[];
+    projects: any[];
+}
+
+export default function Filters({ onFilterChange, countries, types, projects }: FiltersProps) {
+    const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
+    const [selectedType, setSelectedType] = useState<string | null>(null);
+
+    const handleCountryClick = (country: string | null) => {
+        setSelectedCountry(country);
+        onFilterChange({ country, type: selectedType });
+    };
+
+    const handleTypeClick = (type: string | null) => {
+        setSelectedType(type);
+        onFilterChange({ country: selectedCountry, type });
+    };
+    console.log(countries, types, projects)
+    return (
+        <div>
+            <h2>Proyectos</h2>
+            <div className={styles.categories}>
+                <div className={styles.category}>
+                    <h4>Ubicación</h4>
+                    {countries.map((country) => {
+                        const countryCount = projects.filter((project) => project.country === country).length;
+                        return (
+                            <p key={country} onClick={() => handleCountryClick(country)}>
+                                {country} <b>({countryCount})</b>
+                            </p>
+                        );
+                    })}
+                    <p onClick={() => handleCountryClick(null)}>Todos</p>
+                </div>
+                <div className={styles.category}>
+                    <h4>Tipo de cosecha</h4>
+                    {types.map((type) => {
+                        const typesCount = projects.filter((project) => project.seeds.includes(type.toLowerCase())).length;
+                        return (
+                            <p key={type} onClick={() => handleTypeClick(type)}>
+                            {type} <b>({typesCount})</b>
+                            </p>
+                        );
+                    })}
+                    <p onClick={() => handleTypeClick(null)}>Todos</p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- app/(with-navbar)/projects/page.tsx
+++ app/(with-navbar)/projects/page.tsx
@@ -1,41 +1,99 @@
-"use client"
+"use client";
 import ProjectCard from "@/components/projectCard/projectCard";
 import styles from "./projects.module.scss";
 import Filters from "./components/filters/filters";
 import Select from "@/components/select/Select";
 import useProjects from "@/hooks/useProjects";
-import {router} from "next/client";
-import {useRouter} from "next/navigation";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ProjectsPage() {
-  const {projects} = useProjects();
-  const router = useRouter();
+    const { projects } = useProjects();
+    const router = useRouter();
+    const [filteredProjects, setFilteredProjects] = useState(projects);
+    const [order, setOrder] = useState<'recientes' | 'antiguos'>('recientes');
+    const [filters, setFilters] = useState<{ country: string | null; type: string | null }>({
+        country: null,
+        type: null,
+    });
+
+    const countries = Array.from(new Set(projects.map((project) => project.country)));
+    const types = Array.from(new Set(projects.flatMap((project) => project.seeds.map(seed => seed.charAt(0).toUpperCase() + seed.slice(1)))));
+
+    const handleSortChange = (e: any) => {
+        const selectedOrder = e.target.value === "asc" ? "recientes" : "antiguos";
+        setOrder(selectedOrder);
+    };
+
+    const filterProjects = (projects: any[], filters: { country: string | null; type: string | null }) => {
+        return projects.filter((project) => {
+            const matchesCountry = filters.country ? project.country === filters.country : true;
+            const matchesType = filters.type ? project.seeds.includes(filters.type.toLowerCase()) : true;
+            return matchesCountry && matchesType;
+        });
+    };
+
+    function orderProjects(projects: any[], order: 'recientes' | 'antiguos') {
+        return projects.sort((a, b) => {
+            const fechaA = new Date(a.startDate).getTime();
+            const fechaB = new Date(b.startDate).getTime();
+            if (order === 'recientes') {
+                return fechaB - fechaA;
+            } else {
+                return fechaA - fechaB;
+            }
+        });
+    }
+
+    useEffect(() => {
+        const filtered = filterProjects([...projects], filters);
+        const sorted = orderProjects(filtered, order);
+        setFilteredProjects(sorted);
+    }, [order, filters, projects]);
+
+    const handleFilterChange = (newFilters: { country: string | null; type: string | null }) => {
+        setFilters(newFilters);
+    };
 
     const handleCardClick = (projectId: number) => {
         router.push(`/project/${projectId}`);
     };
 
-
     const options = [
-    { value: "asc", title: "Mas recientes" },
-    { value: "desc", title: "Mas antiguos" },
-  ];
-  return (
-    <main className={styles.main}>
-      <section className={styles.sort_section}>
-        <div className={styles.sort_filter}>
-          <span>Ordenar por: </span>
-          <Select placeholder="Mas recientes" name="filter" options={options} />
-        </div>
-      </section>
-      <section className={styles.main_section}>
-        <Filters />
-        <div className={styles.projects_container}>
-          {projects.map((project) => (
-            <ProjectCard project={project} key={project.id} onClick={() =>handleCardClick(project.id)}  />
-          ))}
-        </div>
-      </section>
-    </main>
-  );
+        { value: "asc", title: "Más recientes" },
+        { value: "desc", title: "Más antiguos" },
+    ];
+
+    return (
+        <main className={styles.main}>
+            <section className={styles.sort_section}>
+                <div className={styles.sort_filter}>
+                    <span>Ordenar por: </span>
+                    <Select
+                        placeholder="Más recientes"
+                        name="filter"
+                        options={options}
+                        onChange={handleSortChange}
+                    />
+                </div>
+            </section>
+            <section className={styles.main_section}>
+                <Filters
+                    onFilterChange={handleFilterChange}
+                    countries={countries}
+                    types={types}
+                    projects={projects}
+                />
+                <div className={styles.projects_container}>
+                    {filteredProjects.map((project) => (
+                        <ProjectCard
+                            project={project}
+                            key={project.id}
+                            onClick={() => handleCardClick(project.id)}
+                        />
+                    ))}
+                </div>
+            </section>
+        </main>
+    );
 }
