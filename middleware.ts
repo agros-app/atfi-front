59418d8c269c8c31bc2cef883fe6d932ef3734e6
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-09-22 13:57:09 -0300

    remove console log

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,6 @@ export default async function middleware(req: NextRequest) {
     }
     //Passing the token as parameter because of session problem with cookies
     const completeInfo= await isAuthorized(token!!)
-    console.log(completeInfo)
     if (completeInfo.status != 200){
         return NextResponse.redirect(new URL('/complete-profile', req.nextUrl))
     }
